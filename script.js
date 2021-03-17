c6cc72968061c6d1c82d1405e7e2459728f4094a
Davydkin Valery <st.valeriq@gmail.com>
2021-03-17 19:27:53 +0700

    add sounds

--- script.js
+++ script.js
@@ -6,13 +6,37 @@ document.addEventListener('DOMContentLoaded', () => {
         simbols = ['<-', '=', '+', '-', '*', 'c', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '000'],
         toggleHistoryBtn = document.getElementById('history-btn'),
         resetHistoryBtn = document.getElementById('reset-btn'),
-        tumbler = document.getElementById('tumbler'),
-        lineNumber = 0;
+        theme = document.getElementById('theme'),
+        lineNumber = 0,
+        soundClick = () => {
+            let audio = new Audio('./sounds/click.mp3');
+            return audio.play();
+        },
+        soundError = () => {
+            let audio = new Audio('./sounds/error.mp3');
+            return audio.play();
+        },
+        soundScroll = () => {
+            let audio = new Audio('./sounds/scroll.mp3');
+            return audio.play();
+        },
+        soundReset = () => {
+            let audio = new Audio('./sounds/reset.mp3');
+            return audio.play();
+        },
+        soundDark = () => {
+            let audio = new Audio('./sounds/dark.mp3');
+            return audio.play();
+        },
+        soundLight = () => {
+            let audio = new Audio('./sounds/light.mp3');
+            return audio.play();
+        };
 
     keys.forEach((el) => {
         el.addEventListener('click', () => {
-
             let val = el.getAttribute('data-value');
+            soundClick();
             if (val >= 0 && val <= 9 || val == '+' || val == '-' || val == '*' || val == '/' || val == '.') res.value += val;
 
             if (val === 'c') res.value = '';
@@ -24,17 +48,20 @@ document.addEventListener('DOMContentLoaded', () => {
             if (val === '=' && res.value) {
                 res.value = eval(res.value);
                 if (!isFinite(res.value)) {
-                    window.confirm('Error:\n Deviding on 0 dosn\'t make since');
+                    soundError();
+                    setTimeout(() => {
+                        return window.confirm('Error:\n Deviding on 0 dosn\'t make since');
+                    }, 100);
                     res.value = '';
                 }
             }
 
             historyDefaultLabel.style.display = 'none';
+
             function press(simbols) {
                 simbols.forEach((simbol) => {
                     if (val === simbol) {
                         lineNumber++;
-                        console.log(lineNumber);
                         history.innerHTML += `${lineNumber}| Pressed "${val}"\n`;
                     }
                     if (!history.innerHTML) {
@@ -49,6 +76,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     toggleHistoryBtn.addEventListener('click', (e) => {
         let wrap = document.getElementById('historyWrap');
+        soundScroll();
         wrap.classList.contains('hidden') ?
             wrap.classList.remove('hidden') :
             wrap.classList.add('hidden');
@@ -61,17 +89,20 @@ document.addEventListener('DOMContentLoaded', () => {
             history.innerHTML = '';
             resetHistoryBtn.classList.add('blocked-btn');
             lineNumber = 0;
+            soundReset();
         }
     });
 
-    tumbler.addEventListener('click', () => {
-        let tumblerClass = document.body.classList;
-        if (tumblerClass.contains('dark')) {
-            tumblerClass.remove('dark');
-            tumblerClass.add('light');
+    theme.addEventListener('click', () => {
+        let themeClass = document.body.classList;
+        if (themeClass.contains('dark')) {
+            soundLight();
+            themeClass.remove('dark');
+            themeClass.add('light');
         } else {
-            tumblerClass.remove('light');
-            tumblerClass.add('dark');
+            soundDark();
+            themeClass.remove('light');
+            themeClass.add('dark');
         }
     });
 });
